05561f7de547b08b61b1552f3108f06e619b522d
Amir Zarrinkafsh <nightah@me.com>
2025-07-28 13:31:39 +1000

    refactor: dockerfile adjustments, add lefthook and megalinter
    This change adjusts the `Dockerfile` with enhancements only available in newer Docker and Buildx releases.
    
    I have also added LeftHook and MegaLinter with an appropriately adjusted configuration for linting.

--- dgoss-tests.sh
+++ dgoss-tests.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-
 ### Parse options
 
 # -allow a command to fail with !’s side effect on errexit
 # -use return value from ${PIPESTATUS[0]}, because ! hosed $?
-! getopt --test > /dev/null 
+! getopt --test >/dev/null
 if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
-    echo 'I’m sorry, `getopt --test` failed in this environment.'
-    exit 1
+	echo "I'm sorry, \`getopt --test\` failed in this environment."
+	exit 1
 fi
 
 #Use t: or thing: to get a fileoption
@@ -33,60 +32,60 @@ ENGINE="dgoss"
 # -pass arguments only via   -- "$@"   to separate them correctly
 ! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
 if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
-    # e.g. return value is 1
-    #  then getopt has complained about wrong arguments to stdout
-    exit 2
+	# e.g. return value is 1
+	#  then getopt has complained about wrong arguments to stdout
+	exit 2
 fi
 # read getopt’s output this way to handle the quoting right:
 eval set -- "$PARSED"
 
 # now enjoy the options in order and nicely split until we see --
 while true; do
-    case "$1" in
-        -d|--docker)
-            ENGINE="docker"
-			shift
-			;;
-        -v|--verbose)
-            if [[ "$VERBOSE" == "true" ]]; then
-				DEBUG=true
-			else
-				VERBOSE=true
-			fi
-            shift
-            ;;
-        -c|--circleci)
-            CIRCLECI=true
-			SAVELOG=true
-			LOGFILE="reports/goss/container.log"
-            shift
-            ;;
-        -k|--keepimage)
-            KEEPIMAGE=true
-            shift
-            ;;
-        --edit)
-            DGOSSEDIT=true
-            shift
-            ;;
-        -l|--log)
-            SAVELOG=true
-			LOGFILE="$2"
-            shift 2
-            ;;
-        -i|--imagename)
-			IMAGENAME="$2"
-            shift 2
-            ;;
-        --)
-            shift
-            break
-            ;;
-        *)
-            echo "Programming error"
-            exit 3
-            ;;
-    esac
+	case "${1}" in
+	-d | --docker)
+		ENGINE="docker"
+		shift
+		;;
+	-v | --verbose)
+		if [[ "${VERBOSE}" == "true" ]]; then
+			DEBUG=true
+		else
+			VERBOSE=true
+		fi
+		shift
+		;;
+	-c | --circleci)
+		CIRCLECI=true
+		SAVELOG=true
+		LOGFILE="reports/goss/container.log"
+		shift
+		;;
+	-k | --keepimage)
+		KEEPIMAGE=true
+		shift
+		;;
+	--edit)
+		DGOSSEDIT=true
+		shift
+		;;
+	-l | --log)
+		SAVELOG=true
+		LOGFILE="${2}"
+		shift 2
+		;;
+	-i | --imagename)
+		IMAGENAME="${2}"
+		shift 2
+		;;
+	--)
+		shift
+		break
+		;;
+	*)
+		echo "Programming error"
+		exit 3
+		;;
+	esac
 done
 
 # handle any non-option arguments
@@ -97,74 +96,68 @@ done
 #fi
 
 if [[ "$DEBUG" == "true" ]]; then
-	echo "VERBOSE= $VERBOSE"
-	echo "DEBUG= $DEBUG"
-	echo "CIRCLECI= $CIRCLECI"
-	echo "KEEPIMAGE= $KEEPIMAGE"
-	echo "IMAGENAME=\"$IMAGENAME\""
-	echo "SAVELOG=$SAVELOG"
-	echo "LOGFILE=\"$LOGFILE\""
-	echo "ENGINE=$ENGINE"
-	echo "DGOSSEDIT=$DGOSSEDIT"
+	echo "VERBOSE= ${VERBOSE}"
+	echo "DEBUG= ${DEBUG}"
+	echo "CIRCLECI= ${CIRCLECI}"
+	echo "KEEPIMAGE= ${KEEPIMAGE}"
+	echo "IMAGENAME=${IMAGENAME}"
+	echo "SAVELOG=${SAVELOG}"
+	echo "LOGFILE=${LOGFILE}"
+	echo "ENGINE=${ENGINE}"
+	echo "DGOSSEDIT=${DGOSSEDIT}"
 fi
 
 ### Start Script
 
-
-which goss
-if [ $? -ne 0 ]; then
+if ! command -v goss >/dev/null 2>&1; then
 	echo "Please install goss from https://goss.rocks/install"
 	echo "For a quick auto install run the following"
 	echo "curl -fsSL https://goss.rocks/install | sh"
-	exit $?
+	exit 1
 fi
 
-GOSS_WAIT_OPS="-r 60s -s 1s"
+docker build --tag "${IMAGENAME}" .
 
-docker build --tag "$IMAGENAME" .
-
-if [[ "$CIRCLECI" == "true" ]]; then
+if [[ "${CIRCLECI}" == "true" ]]; then
 	mkdir -p ./reports/goss
-	export GOSS_OPTS="$GOSS_OPTS --format junit"
+	export GOSS_OPTS="${GOSS_OPTS} --format junit"
 fi
 
-export CONTAINER_LOG_OUTPUT="$LOGFILE"
+export CONTAINER_LOG_OUTPUT="${LOGFILE}"
 
-
-if [[ "$CIRCLECI" == "true" ]]; then
-	dgoss run $@ $IMAGENAME > reports/goss/report.xml
+if [[ "${CIRCLECI}" == "true" ]]; then
+	dgoss run "$@" "${IMAGENAME}" >reports/goss/report.xml
 else
-	if [[ "$DGOSSEDIT" == "true" ]]; then
+	if [[ "${DGOSSEDIT}" == "true" ]]; then
 		DCOMMAND=edit
 	else
-		if [[ "$ENGINE" == "docker" ]]; then
+		if [[ "${ENGINE}" == "docker" ]]; then
 			DCOMMAND="run --rm"
 		else
 			DCOMMAND=run
 		fi
 	fi
-	$ENGINE $DCOMMAND $@ $IMAGENAME
+	${ENGINE} "${DCOMMAND}" "$@" "${IMAGENAME}"
 fi
 
 #store result for exit code
 RESULT=$?
 
-
-if [[ "$CIRCLECI" == "true" ]]; then
+if [[ "${CIRCLECI}" == "true" ]]; then
 	echo \
-"Container Output:
+		"Container Output:
 $(cat reports/goss/container.log)" \
-	> reports/goss/container.log
+		>reports/goss/container.log
 	#delete the junk that goss currently outputs :(
 	sed -i '0,/^</d' reports/goss/report.xml
 	#remove invalid system-err outputs from junit output so circleci can read it
 	sed -i '/<system-err>.*<\/system-err>/d' reports/goss/report.xml
 fi
 
-[[ "$VERBOSE" == "true" ]] && cat "$LOGFILE"
+[[ "${VERBOSE}" == "true" ]] && cat "${LOGFILE}"
 
-[[ "$SAVELOG" == "false" ]] && [[ "$ENGINE" == "dgoss" ]] && rm "$LOGFILE"
+[[ "${SAVELOG}" == "false" ]] && [[ "${ENGINE}" == "dgoss" ]] && rm "${LOGFILE}"
 
-[[ "$KEEPIMAGE" == "false" ]] && echo "Deleting docker image" && docker rmi $IMAGENAME
+[[ "${KEEPIMAGE}" == "false" ]] && echo "Deleting docker image" && docker rmi "${IMAGENAME}"
 
-exit $RESULT
+exit ${RESULT}
--- lancache-litmus/overlay/hooks/entrypoint-pre.d/10_generate_litmus_statusjs.sh
+++ lancache-litmus/overlay/hooks/entrypoint-pre.d/10_generate_litmus_statusjs.sh
@@ -1,59 +1,66 @@
 #!/bin/bash
 IFS=' '
 mkdir -p /data/cachedomains
-cd /data/cachedomains
+cd /data/cachedomains || exit 1
 
 sed -i "s/LITMUS_HOSTNAME/${LITMUS_HOSTNAME}/" /etc/nginx/sites-available/litmus.conf.d/10_litmus.conf
 export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostCACHE_IDENTIFIERChecking=no"
 if [[ ! -d .git ]]; then
-	git clone ${CACHE_DOMAINS_REPO} .
+	git clone "${CACHE_DOMAINS_REPO}" .
 fi
 
 if [[ "${NOFETCH:-false}" != "true" ]]; then
 	git fetch origin
-	git reset --hard origin/${CACHE_DOMAINS_BRANCH}
+	git reset --hard "origin/${CACHE_DOMAINS_BRANCH}"
 fi
 
 TEMP_PATH=$(mktemp -d)
 OUTPUTFILE=${TEMP_PATH}/outfile.js
-echo "var cachedomains = {" >> $OUTPUTFILE
-jq -r '.cache_domains | to_entries[] | .key' cache_domains.json | while read CACHE_ENTRY; do 
-	# for each cache entry, find the cache indentifier
-	CACHE_IDENTIFIER=$(jq -r ".cache_domains[$CACHE_ENTRY].name" cache_domains.json)
-	CACHE_DESCRIPTION=$(jq -r ".cache_domains[$CACHE_ENTRY].description" cache_domains.json)
-	jq -r ".cache_domains[$CACHE_ENTRY].domain_files | to_entries[] | .key" cache_domains.json | while read CACHEHOSTS_FILEID; do
-		# Get the key for each domain files
-		echo "	\"${CACHE_IDENTIFIER}\": {" >> $OUTPUTFILE
-		echo "		\"description\": \"${CACHE_DESCRIPTION}\"," >> $OUTPUTFILE
-		echo "		\"domains\": [" >> $OUTPUTFILE
-		jq -r ".cache_domains[$CACHE_ENTRY].domain_files[$CACHEHOSTS_FILEID]" cache_domains.json | while read CACHEHOSTS_FILENAME; do
-			# Get the actual file name
-			cat ${CACHEHOSTS_FILENAME} | while read CACHE_HOST; do
-				# for each file in the hosts file
-				# remove all whitespace (mangles comments but ensures valid config files)
-				CACHE_HOST=${CACHE_HOST// /}
-				if [[ ${CACHE_HOST:0:1} == '#' ]]; then
-					continue;
-				fi
-				if [ ! "x${CACHE_HOST}" == "x" ]; then
-					echo "			\"${CACHE_HOST}\"," >> $OUTPUTFILE
-				fi
-			done
-		done
-		echo "		]" >> $OUTPUTFILE
-		echo "	}," >> $OUTPUTFILE
+echo "var cachedomains = {" >>"${OUTPUTFILE}"
+jq -c '.cache_domains[]' cache_domains.json | while read -r CACHE_ENTRY; do
+	CACHE_IDENTIFIER=$(jq -r '.name' <<<"${CACHE_ENTRY}")
+	CACHE_DESCRIPTION=$(jq -r '.description' <<<"${CACHE_ENTRY}")
+	CACHEHOSTS_FILEID=$(jq -r '.domain_files[]' <<<"${CACHE_ENTRY}")
+
+	cat <<-EOF >>"${OUTPUTFILE}"
+		  "${CACHE_IDENTIFIER}": {
+		    "description": "${CACHE_DESCRIPTION}",
+		    "domains": [
+	EOF
+
+	DOMAINS=()
+	for FILE in ${CACHEHOSTS_FILEID}; do
+		while IFS= read -r CACHE_HOST; do
+			# Remove comments and trim whitespace
+			CACHE_HOST="${CACHE_HOST%%#*}"
+			CACHE_HOST="$(echo "${CACHE_HOST}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+			if [[ -n "${CACHE_HOST}" ]]; then
+				DOMAINS+=("${CACHE_HOST}")
+			fi
+		done <"${FILE}"
 	done
+
+	for ((i = 0; i < ${#DOMAINS[@]}; i++)); do
+		DOMAIN="${DOMAINS[i]}"
+		if ((i < ${#DOMAINS[@]} - 1)); then
+			echo "      \"${DOMAIN}\"," >>"${OUTPUTFILE}"
+		else
+			echo "      \"${DOMAIN}\"" >>"${OUTPUTFILE}"
+		fi
+	done
+	echo "    ]" >>"${OUTPUTFILE}"
+	echo "  }," >>"${OUTPUTFILE}"
 done
-echo "};" >> $OUTPUTFILE
+echo "};" >>"${OUTPUTFILE}"
 if [[ -d .git ]]; then
 	giturl=$(git config --get remote.origin.url)
 	datetime=$(git --no-pager log -1 --format=%cd)
 	commit=$(git --no-pager log -1 --format=%h)
-	echo "var urltext='Running with hostlist from <a target=\"_blank\" href=\"${giturl}\">${giturl}</a> (version ${commit}, ${datetime})';" >> $OUTPUTFILE;
+	echo "var urltext='Running with hostlist from <a target=\"_blank\" href=\"${giturl}\">${giturl}</a> (version ${commit}, ${datetime})';" >>"${OUTPUTFILE}"
 else
 	datetime=$(date)
-	echo "var urltext='Running with an external hostlist that is not git controlled (Generated at ${date})';" >> $OUTPUTFILE;
+	echo "var urltext='Running with an external hostlist that is not git controlled (Generated at ${datetime})';" >>"${OUTPUTFILE}"
 fi
-cat $OUTPUTFILE
-cp $OUTPUTFILE /var/www/litmus/sites.js
-rm -rf $TEMP_PATH
+cat "${OUTPUTFILE}"
+cp "${OUTPUTFILE}" /var/www/litmus/sites.js
+rm -rf "${TEMP_PATH}"
